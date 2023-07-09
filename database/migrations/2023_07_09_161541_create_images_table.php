32c138a3692479bb49e6d503018aff73d6312ea0
Mdsujansarkar <sujansarkar786@gmail.com>
2023-07-09 22:23:10 +0600

    migration add

--- database/migrations/2023_07_09_161541_create_images_table.php
+++ database/migrations/2023_07_09_161541_create_images_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->morphs('resource');
+            $table->string('path');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('images');
     }
 };
--- database/migrations/2023_07_09_162216_create_reservations_table.php
+++ database/migrations/2023_07_09_162216_create_reservations_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('reservations');
     }
 };
